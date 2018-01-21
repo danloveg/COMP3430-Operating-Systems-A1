0664c18b86894f16798267c426f84b74640cb2dd
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-01-21 14:29:39 -0600

    Add skeleton files for shell file I/O

---
+++ ShellCode/shellfileio.c
@@ -0,0 +1,49 @@
+/*
+ * shellfileio.c
+ *
+ * Author: Daniel Lovegrove
+ *
+ * Version: January 21/2018
+ *
+ * Purpose: Support reading from a shell intialization file.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include "shellfileio.h"
+
+/**
+ * Tries to open a shell initialization file called .shell_init in the user's
+ * home directory.
+ * 
+ * @returns int: 0 if file was found and opened, 1 if file not found/error
+ */
+int openShellInitFile() {
+    return 1;
+}
+
+
+/**
+ * Gets one line from the open file.
+ * 
+ * Precondition: OpenShellInitFile() has been called and opened file
+ *     successfully.
+ * 
+ * @param char *str: Points to a string from the file
+ */
+void readFileString(char *str) {
+    str = NULL;
+}
+
+
+/**
+ * Closes the shell intialization file.
+ * 
+ * Precondition: OpenShellInitFile() has been called and opened file
+ *     successfully.
+ * 
+ * @returns int: 0 if file closed, 1 if there was an error.
+ */
+int closeShellInitFile() {
+    return 1;
+}
---
+++ ShellCode/shellfileio.h
@@ -0,0 +1,19 @@
+/*
+ * shellfileio.h
+ *
+ * Author: Daniel Lovegrove
+ *
+ * Version: January 21/2018
+ *
+ * Definitions for shellfileio.c
+ */
+
+#ifndef SHELL_FILE_IO_C
+
+#define SHELL_FILE_IO_C
+
+int openShellInitFile();
+void readFileString(char *str);
+int closeShellInitFile();
+
+#endif
