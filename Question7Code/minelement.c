d8de9f3e17cdb02c53bf2041fb2fee855b23a18e
Daniel Lovegrove <d.lovegrove11@gmail.com>
2018-01-23 19:06:10 -0600

    Make a single thread that returns the minimum
    Practice for creating N threads, each of which find the minimum in their
    specified range.

--- Question7Code/minelement.c
+++ Question7Code/minelement.c
@@ -20,16 +20,20 @@
 typedef struct __START_END {
     int start;
     int end;
-} StartEnd;
+    int *ar;
+} StartEndArray;
 
 void getArgs(int argc, char *argv[], int *numThreads, int *numElts);
 int findMin(unsigned int threadNumElts, unsigned int start, const int *ar);
 void loadArrayFromFile(const char *filename, int *ar, int numElts);
+void *workerThread(void *arg);
 
 // Define static array
 int *ar;
 
 int main (int argc, char *argv[]) {
+    pthread_t p;
+
     // Get arguments
     int numThreads, numElts = 0;
     getArgs(argc, argv, &numThreads, &numElts);
@@ -40,8 +44,34 @@ int main (int argc, char *argv[]) {
     ar = calloc(numElts, sizeof(int));
     loadArrayFromFile(FILENAME, ar, numElts);
 
+    // Create worker thread (test)
+    int *returnValue;
+    StartEndArray threadArg;
+    threadArg.start = 0;
+    threadArg.end = 5;
+    threadArg.ar = ar;
+
+    pthread_create(&p, NULL, workerThread, &threadArg);
+    pthread_join(p, (void **) &returnValue);
+
+    printf("%d returned from thread.\n", (int) *returnValue);
+
+    free(returnValue);
+
     // Clean up
     free(ar);
+}
+
+
+void *workerThread(void *arg) {
+    StartEndArray *my_arg = (StartEndArray *) arg;
+
+    // Find minimum value in array
+    int numElts = my_arg -> end - my_arg -> start;
+    int *returnValue = malloc(sizeof(int));
+    *returnValue = findMin(numElts, my_arg -> start, my_arg -> ar);
+
+    return (void *) returnValue;
 }
 
 
@@ -83,7 +113,8 @@ void loadArrayFromFile(const char *filename, int *ar, int numElts) {
                 element = strtol(characterBuffer, &eptr, 10);
 
                 if (element == 0 && eptr == characterBuffer) {
-                    printf("Error at file line %d, check to make sure val is an int.\nExiting...\n", arIndex + 1);
+                    printf("Error at file line %d, check to make sure val is an int. Exiting...\n", arIndex + 1);
+                    fclose(filePtr);
                     exit(1);
                 } else {
                     ar[arIndex] = element;
@@ -92,7 +123,8 @@ void loadArrayFromFile(const char *filename, int *ar, int numElts) {
             fclose(filePtr);
         }
     } else {
-        printf("Error: Could not open %s\n", filename);
+        printf("Error: Could not open file %s. Exiting...", filename);
+        exit(1);
     }
 }
 
@@ -140,6 +172,10 @@ void getArgs(int argc, char *argv[], int *numThreads, int *numElts) {
         *numElts = strtol(argv[2], &eptr, 10);
         if (*numElts == 0 && eptr == argv[2]) {
             printf("Error converting %s to int. Exiting...\n", argv[2]);
+            exit(1);
+        }
+        if (*numElts % *numThreads != 0) {
+            printf("Number of threads must evenly divide number of elements. Exiting...\n");
             exit(1);
         }
     }
